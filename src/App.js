002b57cfe181b6a15227168308984cd13483dff1
Anton Wyrowski <anton.wyrowski@gmail.com>
2021-12-03 12:14:59 +0100

    added parameter parsing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import { Layout, Menu, Form, Input, Button, Row, Space, message } from 'antd';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+import Converter from './generator';
+import ReactJson from 'react-json-view';
+
+const { Header, Content, Footer } = Layout;
+
+const App = () => {
+  const [json, setJson] = useState(null);
+
+  const onFinish = (data) => {
+    console.log(data);
+
+    message.success('Generated JSON!');
+
+    setJson(Converter(data.json));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="layout">
+      <Header>
+        <h3 style={{ color: 'white' }}>MJML2JSON</h3>
+        <Menu theme="dark" mode="horizontal"></Menu>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <div className="site-layout-content">
+          <h1>Convert your MJML to our JSON:</h1>
+          <Form name="mjml2json" onFinish={onFinish} requiredMark={false}>
+            <Form.Item
+              name="json"
+              label="MJML"
+              rules={[{ required: true, message: 'Please input your MJML!' }]}
+            >
+              <Input.TextArea
+                id="text-area"
+                autoSize={{ minRows: 10, maxRows: 20 }}
+                placeholder="Paste in your MJML"
+              />
+            </Form.Item>
+            <Space style={{ paddingBottom: '20px' }} align="center">
+              <Form.Item style={{ padding: 0, margin: 0 }}>
+                <Button type="primary" htmlType="submit">
+                  Generate
+                </Button>
+              </Form.Item>
+              {json ? (
+                <Button
+                  onClick={async () => {
+                    await navigator.clipboard.writeText(
+                      JSON.stringify(json, null, 2)
+                    );
+
+                    message.success('Copied to clipboard!');
+                  }}
+                  type="secondary"
+                >
+                  Copy JSON
+                </Button>
+              ) : (
+                <div></div>
+              )}
+            </Space>
+          </Form>
+
+          {json ? <ReactJson name={null} src={json} /> : <div></div>}
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Anton Wyrowski ©2021 Created for iPraktikum
+      </Footer>
+    </Layout>
   );
-}
+};
 
 export default App;
---
+++ src/generator.js
@@ -0,0 +1,99 @@
+import mjml2json from 'mjml2json';
+
+let idCounter = 0;
+
+const converter = (json) => {
+  const mjmlGeneratorOutput = mjml2json(json);
+
+  console.log(mjmlGeneratorOutput);
+
+  return convertLayout(mjmlGeneratorOutput);
+};
+
+export default converter;
+
+function convertLayout(mjml) {
+  return { layout: convertNode(mjml) };
+}
+
+function convertNode({ tagName, attributes, children, content }) {
+  const obj = {
+    name: tagName + '-' + String(idCounter++),
+    tag: tagName,
+    contentType: toContentType(tagName),
+    parameters: Object.entries(attributes).map(([key, value]) => {
+      const param = {};
+
+      param.name = key;
+
+      console.log(key);
+
+      switch (key) {
+        case 'font-size':
+        case 'width':
+        case 'border-radius':
+        case 'height':
+        case 'inner-padding':
+        case 'letter-spacing':
+        case 'line-height':
+          if (value.includes('px')) {
+            param.value = Number(value.replace('px', ''));
+            param.valueSuffix = 'px';
+          } else if (value.includes('%')) {
+            param.value = Number(value.replace('%', ''));
+            param.valueSuffix = '%';
+          }
+
+          param.changeable = 'number';
+          break;
+
+        case 'text-align':
+        case 'align':
+          param.changeable = 'alignEnum';
+          param.value = value;
+          break;
+
+        case 'color':
+        case 'background-color':
+          param.changeable = 'color';
+          param.value = value;
+          break;
+
+        default:
+          param.value = value;
+          break;
+      }
+
+      return param;
+    }),
+    children: children?.map(convertNode) ?? [],
+  };
+
+  addContent(obj, content);
+
+  return obj;
+}
+
+function addContent(json, content) {
+  console.log(json);
+  switch (json.tag) {
+    case 'mj-text':
+      json.textContent = content;
+      break;
+    default:
+      return;
+  }
+}
+
+function toContentType(tag) {
+  switch (tag) {
+    case 'mj-text':
+      return 'text';
+    case 'mj-image':
+      return 'image';
+    case 'mj-button':
+      return 'button';
+    default:
+      return 'none';
+  }
+}
